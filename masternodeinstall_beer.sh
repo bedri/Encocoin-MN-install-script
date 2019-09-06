d285733041602d33a0a91da4d844dcc1f3b1f44d
Bedri Özgür Güler <bedriguler@gmail.com>
2019-09-07 02:12:12 +0300

    fix

--- masternodeinstall_beer.sh
+++ masternodeinstall_beer.sh
@@ -335,7 +335,7 @@ DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" -o D
 apt install -y software-properties-common >/dev/null 2>&1
 echo -e "${PURPLE}Adding bitcoin PPA repository"
 apt-add-repository -y ppa:bitcoin/bitcoin >/dev/null 2>&1
-echo -e "Installing required packages, it may take some time to finish.${NC}"
+echo -e "Installing required packages, it may take some time to finish. Go get and drink one more ${LICON} ${NC}"
 apt-get update >/dev/null 2>&1
 apt-get install libzmq3-dev -y >/dev/null 2>&1
 apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" make software-properties-common \
