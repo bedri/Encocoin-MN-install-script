25cc59de6ba6bbe7aec803173b5cfb45b2dec19b
Bedri Özgür Güler <bedriguler@gmail.com>
2019-09-06 20:25:08 +0300

    fix

--- masternodeinstall_verbose_and_debug.sh
+++ masternodeinstall_verbose_and_debug.sh
@@ -101,6 +101,8 @@ EOF
   sleep 3
   systemctl start $COIN_NAME.service
   systemctl enable $COIN_NAME.service
+  echo -e "${GREEN}Encocoin PoS (XNK-PoS) Service Status\n${NC}"
+  systemctl status $COIN_NAME.service
 
   if [[ -z "$(ps axo cmd:100 | egrep $COIN_DAEMON)" ]]; then
     echo -e "${RED}$COIN_NAME is not running${NC}, please investigate. You should start by running the following commands as root:"
