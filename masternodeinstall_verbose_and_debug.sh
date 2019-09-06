748f55dc833860ee404b360384fd83b4ca487334
Bedri Özgür Güler <bedriguler@gmail.com>
2019-09-07 00:08:01 +0300

    fix

--- masternodeinstall_verbose_and_debug.sh
+++ masternodeinstall_verbose_and_debug.sh
@@ -325,14 +325,14 @@ echo -e "\n${BLUE}==============================================================
 EOF
 chmod +x /root/encocoin-masternode-stats
 
-cp /root/.bashrc /root/.bashrc.backup
-sed '/feestats/d' /root/.bashrc | sed '/networkstats/d' | sed '/mnstats/d' | sed '/encocoininfo/d' > /root/tmp
-mv /root/tmp /root/.bashrc
-echo -e "alias encocoininfo='/root/encocoin-networkinfo'" >> /root/.bashrc
-echo -e "alias feestats='/root/encocoin-fee-info'" >> /root/.bashrc
-echo -e "alias networkstats='/root/encocoin-networkinfo'" >> /root/.bashrc
-echo -e "alias mnstats='/root/encocoin-masternode-stats'" >> /root/.bashrc
-$( . /root/.bashrc )
+cp /etc/profile /etc/profile.backup
+sed '/feestats/d' /etc/profile | sed '/networkstats/d' | sed '/mnstats/d' | sed '/encocoininfo/d' > /root/tmp
+mv /root/tmp /etc/profile
+echo -e "alias encocoininfo='/root/encocoin-networkinfo'" >> /etc/profile
+echo -e "alias feestats='/root/encocoin-fee-info'" >> /etc/profile
+echo -e "alias networkstats='/root/encocoin-networkinfo'" >> /etc/profile
+echo -e "alias mnstats='/root/encocoin-masternode-stats'" >> /etc/profile
+. /etc/profile
 # cd -
 }
 
