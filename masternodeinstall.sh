42664f04b204ce3d0f95356dde24d4620ee1115a
Bedri Özgür Güler <bedriguler@gmail.com>
2019-09-06 23:38:03 +0300

    fix

--- masternodeinstall.sh
+++ masternodeinstall.sh
@@ -30,7 +30,7 @@ MAG='\e[1;35m'
 purgeOldInstallation() {
     echo -e "${YELLOW}Searching and removing old ${BLUE}$PROJECT_NAME Masternode ${YELLOW}files and configurations${NC}"
 	#stopping service
-	systemctl stop $COIN_NAME.service
+	# systemctl stop $COIN_NAME.service
     #kill wallet daemon
 	killall $COIN_DAEMON > /dev/null 2>&1
     #remove old ufw port allow
